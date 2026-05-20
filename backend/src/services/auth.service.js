8c5524c3f2feaa1666cd438a74f6de3c38449c8f
benjaminibanez2201 <benjamin.ibanez2201@alumnos.ubiobio.cl>
2026-05-20 19:09:56 -0400

    fix: forgotPasswordService envia correo

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { AppDataSource } from "../config/configDb.js";
 import { comparePassword, encryptPassword } from "../helpers/bcrypt.helper.js";
 import { ACCESS_TOKEN_SECRET } from "../config/configEnv.js";
+import { sendRecoveryEmail } from "./email.service.js";
 
 export async function loginService(user) {
   try {
@@ -101,4 +102,35 @@ export async function registerService(user) {
     return [null, "Error interno del servidor"];
   }
 }
+// funcion para solicitar la recuperacion de contraseña
+export async function forgotPasswordService(email) {
+  try {
+    const userRepository = AppDataSource.getRepository(User);
 
+    const userFound = await userRepository.findOne({
+      where: { email }
+    });
+
+    if (!userFound) {
+      return [null, "Se envió un correo electrónico a la dirección proporcionada si existe una cuenta asociada. Si no recibes un correo, por favor verifica tu dirección de correo electrónico o contacta al soporte. Gracias por tu comprensión."];
+    }
+
+    // generar el token de restablecimiento de contraseña
+
+    const resetToken = jwt.sign({ id: userFound.id }, ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+
+    // Guardar en usuario
+    userFound.resetPasswordToken = resetToken;
+    userFound.resetPasswordExpires = new Date(Date.now() + 3600000); // 1 hora
+
+    await userRepository.save(userFound);
+
+    // enviar el correo con el token de restablecimiento 
+    await sendRecoveryEmail(userFound.email, resetToken);
+
+    return ["Instrucciones para restablecer la contraseña han sido enviadas a tu correo electrónico", null];
+  } catch (error) {
+    console.error("Error al solicitar restablecimiento de contraseña:", error);
+    return [null, "Error interno del servidor"];
+  }
+}
