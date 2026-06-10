02fd1dfaae9e0dea8a64c65acd1d56ad3062e8fa
Javiera <javiera.carrasco2201@alumnos.ubiobio.cl>
2026-06-09 21:10:59 -0400

    agregar ruta /profile para arrendador

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -46,7 +46,7 @@ const router = createBrowserRouter([
       {
         path: '/profile',
         element: (
-          <ProtectedRoute allowedRoles={['estudiante']}>
+          <ProtectedRoute allowedRoles={['estudiante', 'arrendador']}>
             <Profile />
           </ProtectedRoute>
         ),
