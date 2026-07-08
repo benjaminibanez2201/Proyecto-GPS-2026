7e572e6195669d7e094fb939617ed7c588969fc7
Cata <catalina.munoz2201@alumnos.ubiobio.cl>
2026-07-08 02:23:07 -0400

    elimina archivo de entidad favpublicacion que era codigo muerto

--- backend/src/repositories/publicacion.repository.js
+++ backend/src/repositories/publicacion.repository.js
@@ -1,6 +1,5 @@
 "use strict";
 import Publicacion from "../entity/publicacion.entity.js";
-import FavoritoPublicacion from "../entity/favorito_publicacion.entity.js";
 import { AppDataSource } from "../config/configDb.js";
 
 function obtenerManagerRepositorio(manager) {
@@ -25,57 +24,6 @@ export async function obtenerPublicacionBloqueadaRepositorio(manager, id_publica
   }
 }
 
-export async function obtenerFavoritoRepositorio(manager, id_publicacion, usuarioId) {
-  try {
-    const contexto = obtenerManagerRepositorio(manager);
-    const repositorioFavorito = contexto.getRepository(FavoritoPublicacion);
-
-    const favorito = await repositorioFavorito.findOne({
-      where: {
-        publicacion: { id_publicacion },
-        usuario: { id: usuarioId },
-      },
-      relations: ["publicacion", "usuario"],
-    });
-
-    return [favorito, null];
-  } catch (error) {
-    console.error("Error obtenerFavoritoRepositorio:", error);
-    return [null, "Error interno del servidor"];
-  }
-}
-
-export async function crearFavoritoRepositorio(manager, publicacion, usuarioId) {
-  try {
-    const contexto = obtenerManagerRepositorio(manager);
-    const repositorioFavorito = contexto.getRepository(FavoritoPublicacion);
-
-    const favorito = repositorioFavorito.create({
-      publicacion,
-      usuario: { id: usuarioId },
-    });
-
-    const guardado = await repositorioFavorito.save(favorito);
-    return [guardado, null];
-  } catch (error) {
-    console.error("Error crearFavoritoRepositorio:", error);
-    return [null, "Error interno del servidor"];
-  }
-}
-
-export async function eliminarFavoritoRepositorio(manager, favorito) {
-  try {
-    const contexto = obtenerManagerRepositorio(manager);
-    const repositorioFavorito = contexto.getRepository(FavoritoPublicacion);
-
-    await repositorioFavorito.remove(favorito);
-    return [true, null];
-  } catch (error) {
-    console.error("Error eliminarFavoritoRepositorio:", error);
-    return [null, "Error interno del servidor"];
-  }
-}
-
 export async function guardarPublicacionRepositorio(manager, publicacion) {
   try {
     const contexto = obtenerManagerRepositorio(manager);
@@ -91,8 +39,5 @@ export async function guardarPublicacionRepositorio(manager, publicacion) {
 
 export default {
   obtenerPublicacionBloqueadaRepositorio,
-  obtenerFavoritoRepositorio,
-  crearFavoritoRepositorio,
-  eliminarFavoritoRepositorio,
   guardarPublicacionRepositorio,
 };
